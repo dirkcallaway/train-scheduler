7670fc2cd90b07d27a5e5734116c607d9449dcb4
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-01-13 17:28:50 -0700

    Added moment.js functionality.  Works when new train is added.

--- assets/js/trainJS.js
+++ assets/js/trainJS.js
@@ -21,6 +21,7 @@ var timeFormat = "HH:mm";
 
 $(document).ready(function () {
 
+    //When submit button is pushed...
     $("#submit").on("click", function (event) {
         event.preventDefault();
 
@@ -46,28 +47,35 @@ $(document).ready(function () {
 
         //Train Confirmation Pop-up
         $('#trainAdded').modal('show');
-        
+        //Closes comfirmation Pop-up after 1.5 seconds
         setTimeout(function() {
             $('#trainAdded').modal('hide');
         }, 1500);
 
     });
 
+    //Updates HTML when new DB entry received
     database.ref().on("child_added", function(snapshot){
         trainName = snapshot.val().trainName;
-        console.log(trainName);
         destination = snapshot.val().destination;
-        console.log(destination);
         firstTrain = snapshot.val().firstTrain;
-        console.log(firstTrain);
         frequency = snapshot.val().frequency;
-        console.log(frequency);
+
+        //Math to determine next train and minutes away
+        var firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+        var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+        var tRemainder = diffTime % frequency;
+        var minutesTillTrain = frequency - tRemainder;
+        console.log("Minutes til next train: " + minutesTillTrain);
+        var nextTrain = moment().add(minutesTillTrain, "minutes").format("HH:mm");
+
+        //Creates table elements and adds train information from DB to HTML Table
         var tableRow = $("<tr>");
         var tableDataName = $("<td>").text(trainName);
         var tableDataDestination = $("<td>").text(destination);
         var tableDataFrequency = $("<td>").text(frequency);
-        var tableDataNext = $("<td>").text("n/a");
-        var tableDataMinutesAway = $("<td>").text("n/a");
+        var tableDataNext = $("<td>").text(nextTrain);
+        var tableDataMinutesAway = $("<td>").text(minutesTillTrain);
         tableRow.append(tableDataName);
         tableRow.append(tableDataDestination);
         tableRow.append(tableDataFrequency);
